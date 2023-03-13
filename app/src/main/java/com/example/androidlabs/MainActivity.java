ce4e1f485d6919ba290686a3f4a7b8ec7cece2e0
Will-Rakita <holorits@gmail.com>
2023-03-13 17:44:29 -0400

    Lab 2 Complete stuff

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button)findViewById(R.id.button);
         EditText edt = (EditText)findViewById(R.id.editText) ;
         TextView txt = (TextView)findViewById(R.id.textView) ;
-        btn.setOnClickListener((click) -> txt.setText(edt.getText()));
+        CheckBox chk = (CheckBox)findViewById(R.id.checkbox);
+        btn.setOnClickListener((click) -> {txt.setText(edt.getText());
+
+            Toast.makeText(MainActivity.this, R.string.toast_message_lab2 , Toast.LENGTH_LONG ).show();
+        });
+
+        chk.setOnCheckedChangeListener((cb, isChecked) -> {
+            Snackbar.make(chk, "Checkbox is " + isChecked, Snackbar.LENGTH_LONG)
+                    .setAction("Undo", click-> chk.setChecked(!isChecked))
+                    .show();
+        });
+
+
+
+
+
     }
+
 }
