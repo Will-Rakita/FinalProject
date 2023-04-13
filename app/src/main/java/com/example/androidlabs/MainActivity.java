eaafbc98ba09cfbc12e60d7b4a1f62fe12c9cec7
Will Rakita <holorits@gmail.com>
2023-04-12 21:19:11 -0400

    Continuing with lab 6

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +23,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        CatImages req = new CatImages();
+        req.execute("https://cataas.com/cat?json=true");
     }
     private class CatImages extends AsyncTask<String, Integer, String> {
-
+        private Bitmap nerd;
         @Override
         protected String doInBackground(String... strings) {
-            Bitmap nerd;
+            try {
 
-            return null;
+                //create a URL object of what server to contact:
+                URL url = new URL(strings[0]);
+
+                //open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //wait for data:
+                InputStream response = urlConnection.getInputStream();
+
+
+                //JSON reading:
+                //Build the entire string response:
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null)
+                {
+                    sb.append(line + "\n");
+                }
+                String result = sb.toString(); //result is the whole string
+
+
+                // convert string to JSON:
+                JSONObject uvReport = new JSONObject(result);
+
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return "Done";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... args) {
+
+        }
+
+        @Override
+        protected void onPostExecute(String fromDoInBackground) {
+            Log.i("HTTP", fromDoInBackground);
         }
     }
 }
