6013415866efb88118aff309bb6bf9165e884ef0
Will Rakita <holorits@gmail.com>
2023-04-20 21:01:13 -0400

    Lab 7, 5 done, could be better.

--- app/src/main/java/com/example/androidlabs/DetailsFragment.java
+++ app/src/main/java/com/example/androidlabs/DetailsFragment.java
@@ -1,20 +1,41 @@
 package com.example.androidlabs;
 
+
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 
 public class DetailsFragment extends Fragment {
+    private boolean isTablet;
+    private Bundle dataFromActivity;
+    private long id;
 
+    public void setTablet(boolean tablet) { isTablet = tablet; }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        dataFromActivity = getArguments();
+
+
+
+        // Inflate the layout for this fragment
+        View result =  inflater.inflate(R.layout.fragment_details, container, false);
+
+
+
+
+
+
         return inflater.inflate(R.layout.fragment_details, container, false);
+
     }
 }
--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Bundle dataToPass = getIntent().getExtras(); //get the data that was passed from FragmentExample
+
+        //This is copied directly from FragmentExample.java lines 47-54
+        DetailsFragment dFragment = new DetailsFragment();
+        dFragment.setArguments( dataToPass ); //pass data to the the fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentlocation, dFragment)
+                .commit();
     }
 }
