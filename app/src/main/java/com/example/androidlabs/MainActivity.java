0e678d2cfcaeaef62a0d4e84f490aa6569ecfaa9
Will Rakita <holorits@gmail.com>
2023-05-05 22:31:02 -0400

    Final Project Start

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -13,17 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Using apt compact libraries
-        Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
-        //calls oncreatoptionsmenue
-        setSupportActionBar(myToolbar);
 
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-// Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.example_menu, menu);
-        return true;
+
     }
 }
